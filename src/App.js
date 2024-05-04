fcde73a86c2a751eafece0fd6a5849d6d6f89d32
Aditya singh <adityasingh@Adityas-Laptop.local>
2024-05-04 12:33:34 +0530

    testing changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <Sidebar />
-      
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
